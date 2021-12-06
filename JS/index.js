bcce3f5884e23e61b82bfcdbeacccc05fbd18521
Lizzie Anthony <lizziewrab@me.com>
2021-12-06 16:31:52 -0600

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", 90 => {
+    const addBtn = document.querySelector("#recipe-button")
+})
+
+fetch("https://api.disneyapi.dev/characters")
+
+//  Globals //
+
+const containerDiv = () => document.getElementById("container");
+
+
+// Templates ??
+
+const homePageTemplate= () => {
+    return ``
+}
+
+// Node Getters //
+
+
+
